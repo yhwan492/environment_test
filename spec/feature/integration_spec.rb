f0481d7c367b26823dc220d2d45106400a296481
yhwan492 <61440704+yhwan492@users.noreply.github.com>
2022-09-16 14:45:35 +0900

    add files third time

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    select('2022-01-01', from: 'Published date')
+    fill_in 'Price', with: 1.0
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'jk rowling', price: 1.0, published_date: '2022-01-01')
   end
 
   it 'is valid with valid attributes' do
@@ -13,5 +13,19 @@ RSpec.describe Book, type: :model do
   it 'is not valid without a name' do
     subject.title = nil
     expect(subject).not_to be_valid
+  end
+  it 'is valid without an author' do
+    subject.author = nil
+    expect(subject).to be_valid
+  end
+
+  it 'is valid without a price' do
+    subject.price = nil
+    expect(subject).to be_valid
+  end
+
+  it 'is valid without a datetime' do
+    subject.published_date = nil
+    expect(subject).to be_valid
   end
 end
