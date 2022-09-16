8f7dd52f282f70d15b75a441e2490ad8ee752e47
yhwan492 <kyhwna492@gmail.com>
2022-09-16 14:25:15 -0500

    added User, userbooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :books
-  root 'books#index'
+  resources :user_books
+  resources :users
+  
+  root 'user_books#index'
 
-  # get 'books/index'
-
-  # get ':controller(/:action(/:id))'
+  resources :books do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_053819) do
+ActiveRecord::Schema.define(version: 2022_09_16_181615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2022_09_16_053819) do
     t.string "author"
     t.date "published_date"
     t.float "price"
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
