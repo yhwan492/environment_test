1236cb6e21e34e9ac6779e0de33b48bd8e510015
yhwan492 <61440704+yhwan492@users.noreply.github.com>
2022-09-16 15:08:59 +0900

    update book controller
    omitted lines during merging

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -66,6 +66,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title,:author,:published_date,:price)
     end
 end
